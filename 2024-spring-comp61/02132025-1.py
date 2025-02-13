0780dbdc16d6c3179bbf0d789f25edd9a3488f5b
Xiangxu Lin <cricel.design@gmail.com>
2025-02-13 08:19:05 -0800

    add

---
+++ 2024-spring-comp61/02132025-1.py
@@ -0,0 +1,24 @@
+base = 4
+input_raw = input("enter a value: ")
+print("1. add")
+print("2. minus")
+print("3. print")
+print("4. quit")
+
+while (input_raw != 4):
+    if(input_raw == 1):
+        result = -1;
+        result = 3 + base;
+        print(result)
+    elif(input_raw == 2):
+        result = -1
+        result = base - 5
+        print(result)
+    elif(input_raw == 3):
+        print(base)
+
+    input_raw = input("enter a value: ")
+    print("1. add")
+    print("2. minus")
+    print("3. print")
+    print("4. quit")
---
+++ 2024-spring-comp61/02132025-2.py
@@ -0,0 +1,36 @@
+
+# a = 3
+# a = a + 3
+# print(a)
+
+
+# def add():
+#     a = 3
+#     a = a + 3
+#     print(a)
+
+# add()
+
+# def add():
+#     a = 3
+#     a = a + 3
+#     return a
+
+# print(add())
+
+
+# def add():
+#     global a
+#     a = a + 3
+
+# a = 3
+# add()
+# print(a)
+
+def add(aa):
+    # aa = <<<
+    aa = aa + 3
+    return aa
+
+a = 3
+print(add(45))
