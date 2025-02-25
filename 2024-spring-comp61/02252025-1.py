22fda25fa0a2dbe91c5a19f69d0d508423050b67
Xiangxu Lin <cricel.design@gmail.com>
2025-02-25 09:13:40 -0800

    update

---
+++ 2024-spring-comp61/02252025-1.py
@@ -0,0 +1,110 @@
+# a = 1
+
+# def test():
+#     a = a+1
+#     a= 10 -3
+#     return 10
+
+
+# class TestClass:
+#     a = 3
+#     def test():
+#         return 2
+    
+
+# print(a)
+# print(test())
+
+# testclass = TestClass()
+# print(testclass.a)
+# print(testclass.test())
+
+
+# a = 3
+# c = a + 5
+
+# a1 = 4
+# c = a1 + 5
+
+# a2 = 6
+# c = a2 + 5
+
+# def cal(x):
+#     return x + 5
+
+# a = 3
+# c = cal(a)
+# a1 = 4
+# c = cal(a1)
+# # print(c)
+# a2 = 6
+# c = cal(a2)
+# print("----------")
+# a = 4
+# cal():
+#     xxxx
+# class abc:
+#     a = 2
+#     b = 4
+#     c = [1,2,3]
+
+#     def call(self, x):
+#         return self.b + 5 + x
+    
+# abcClass = abc()
+# print(abcClass.a)
+# abcClass.a = 10
+# print(abcClass.a)
+# print(abcClass.call(10))
+# aaclass = abc()
+# print (aaclass.a)
+# print(aaclass.call())
+
+class Person:
+    name = "testUser"
+    age = -1
+    job = "none"
+
+    def salary(self):
+        return self.age * 10
+    
+shawnPerson = Person()
+shawnPerson.name = "shawn"
+shawnPerson.age = 10
+shawnPerson.job = "programmer"
+print(shawnPerson.salary())
+print(shawnPerson.name)
+print(shawnPerson.age)
+print(shawnPerson.job)
+TinaPerson = Person()
+TinaPerson.name = "Tina"
+TinaPerson.age = 9
+TinaPerson.job = "hihi"
+print(TinaPerson.salary())
+print(TinaPerson.name)
+print(TinaPerson.age)
+print(TinaPerson.job)
+BenPerson = Person()
+BenPerson.age = 8
+print(TinaPerson.salary())
+
+#create a class call person, with a variable name call "name", "age", "job"
+#inside class, create another function called "salary"
+#in salary function, the salary is equal to age * 10
+# create 3 instance of this person class, with different name and age and job, and print out its salary
+
+
+
+
+# def test(x):
+#     a = 3
+#     a  = a +x
+#     return a
+# def test1():
+#     a = 3
+#     a  = a +5
+#     return a
+# a = test1()
+# b=test1()
+# # a = test(4)
+# # b = test(10)
