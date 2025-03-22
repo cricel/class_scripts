552028f1e46adc6699a93e217d125eff8603ef7b
Xiangxu Lin <cricel.design@gmail.com>
2025-03-22 16:14:45 -0700

    update

--- 2025-spring-comp61/notes/03202025-1.py
+++ 2025-spring-comp61/notes/03202025-1.py
@@ -1,23 +1,40 @@
-couter = 10
+# couter = 10
 
-def count_down(count):
-    global couter
-    if count == 0:  
-        couter = count     
-        print('Go!')                  
-    else:                        
-        print(count)             
-        count_down(count-1) 
+# def count_down(count):
+#     global couter
+#     if count == 0:  
+#         couter = count     
+#         print('Go!')                  
+#     else:                        
+#         print(count)             
+#         count_down(count-1) 
 
-def count_up(count):
-    global couter
-    if count == 10: 
-        couter = count            
-        print('Go!')                  
-    else:                        
-        print(count)             
-        count_up(count+1) 
+# def count_up(count):
+#     global couter
+#     if count == 10: 
+#         couter = count            
+#         print('Go!')                  
+#     else:                        
+#         print(count)             
+#         count_up(count+1) 
 
-count_down(couter)
-count_up(couter) 
-## count down from 10 to 0 and count up to 10
+# count_down(couter)
+# count_up(couter) 
+# ## count down from 10 to 0 and count up to 10
+
+def count(couter, down): ## count down from 10 to 0 and count up to 10
+    if couter == 0:   
+        down = False   
+        count(couter + 1, down)
+    elif couter == 10 and down == False:
+        print('Done!') 
+    else: 
+        if(down == True):                       
+            print(couter)             
+            count(couter-1, down) 
+        else:
+            print(couter)             
+            count(couter+1, down) 
+
+
+count(10, True)
