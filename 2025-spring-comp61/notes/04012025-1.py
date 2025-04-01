5eb9b4bed841e55a4974b828a1dcc5fba53184cb
Xiangxu Lin <cricel.design@gmail.com>
2025-04-01 08:23:35 -0700

    Create 04012025-1.py

---
+++ 2025-spring-comp61/notes/04012025-1.py
@@ -0,0 +1,34 @@
+a = [1,1,2,3,"shawn", True]
+print(a[0])
+print(a[2])
+
+students = {}
+students ['Jose'] = {'Grade': 'A+', 'StudentID': 22321}
+students ['Shawn'] = {'Grade': 'F', 'StudentID': 00000}
+
+print(students)
+print('Jose:')
+
+print(f' Grade: {students ["Jose"]["Grade"]}')
+
+print(f' ID: {students["Jose"]["StudentID"]}')
+# write a dict to have a collection of car name and price,
+# and print out the price of all the s
+
+class Test:
+    a = 1
+phone_price = {}
+phone_price['iphone'] = [1000, 200]
+phone_price['samsung'] = 800
+phone_price['oneplus'] = Test()
+
+print(phone_price)
+print(phone_price['samsung'])
+print(phone_price['iphone'])
+
+# price_list = [1000, 800, 600]
+# phone_list = ['iphone', 'samsung', 'oneplus']
+
+# print(phone_list[0], price_list[0])
+
+
