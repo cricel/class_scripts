299439212f168fe86ba7f9e22800e9470c9a3310
Xiangxu Lin <cricel.design@gmail.com>
2025-04-03 15:12:45 -0700

    Create quiz05.cpp

---
+++ 2025-spring-comp51/notes/quiz05.cpp
@@ -0,0 +1,51 @@
+// #include <iostream>
+// using namespace std;
+
+// void sum_region(int two_d_arr[3][3], int row, int& total){
+//     for (int i=0; i< 3; i++){
+//         // cout << two_d_arr[row][i];
+//         total += two_d_arr[row][i];
+//     }
+
+//     if(row < 2){
+//         sum_region(two_d_arr, row + 1, total);
+//     }
+// }
+
+// int main() {
+//     int two_d_arr[3][3]= {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9},
+//     };
+
+//     int total = 0;
+//     sum_region(two_d_arr, 0, total);
+
+//     cout << total;
+// }
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void sum_region(xxxxxxxxxxxxxxx){
+    // calculate the total of each row,
+    // use recusive function add up each row
+}
+
+int main() {
+    int two_d_arr[3][3]= {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    int total = 0;
+    
+    sum_region(xxxxxxxxxxxxxxx);
+
+    cout << total;
+}
